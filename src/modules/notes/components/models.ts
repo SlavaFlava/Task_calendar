792feda88f108b646f80d02e2abb0289758332e6
SlavaFlava <parxomenko.90@inbox.ru>
2023-06-21 12:44:15 +0600

    add MDEditor for notes

--- src/modules/notes/components/models.ts
+++ src/modules/notes/components/models.ts
@@ -1,14 +1,18 @@
 export interface INote {
-    parent: string,
-    noteUrl: string,
-    title: string,
-    body: string,
-    date: string,
+  parent: string,
+  noteUrl: string,
+  title: string,
+  body: string,
+  date: string,
 }
 export interface ICategory {
-    icon: number,
-    categoryUrl?: string,
-    name: string,
-    color: string,
-    notes: INote[]
+  icon: number,
+  categoryUrl?: string,
+  name: string,
+  color: string,
+  notes: INote[]
+}
+export interface IBody {
+  color: string,
+  body: string
 }
--- src/modules/notes/note-details.tsx
+++ src/modules/notes/note-details.tsx
@@ -6,6 +6,7 @@ import IconsArray from './components/icons';
 import ChevronLeft from '@mui/icons-material/ChevronLeft';
 import ChevronRight from '@mui/icons-material/ChevronRight';
 import CategoryNotes from './components/category';
+import NoteBody from './components/note-body';
 
 const NoteDetails = () => {
 
@@ -17,6 +18,8 @@ const NoteDetails = () => {
   return (
     <Paper variant={'outlined'} square
       sx={{ p: '22px', borderRadius: '10px', position: 'relative', boxShadow: `4px 4px 14px -10px ${'grey'}` }}>
+
+
       <Box display={'flex'} alignItems={'center'}>
         <SvgIcon sx={{ color: category.color, fontSize: '36px', mr: 1 }}>
           {IconsArray[category.icon]}
@@ -30,6 +33,12 @@ const NoteDetails = () => {
           {note.title}
         </Typography>
       </Box>
+       
+
+       <NoteBody color={category.color} body={note.body}/>
+
+
+
       <Box display={'flex'} alignItems={'center'} justifyContent={'space-between'} mb={1} mt={3}>
         {
           category.notes[indexNote - 1]
@@ -47,7 +56,7 @@ const NoteDetails = () => {
         {
           category.notes[indexNote + 1]
             ?
-            <NavLink  to={`../${category.notes[indexNote + 1].noteUrl}`}>
+            <NavLink to={`../${category.notes[indexNote + 1].noteUrl}`}>
               <Typography color={category.color} alignItems={'center'} display={'flex'} fontWeight={'600'} textTransform={'uppercase'}>
                 Next Note <ChevronRight />
               </Typography>
